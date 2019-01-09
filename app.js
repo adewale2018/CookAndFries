569e143216fcc5242b4c1706a10088bc3471ad5d
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-01-09 19:37:51 +0100

    use css to style the index page

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ const   commentRoutes = require('./routes/comments'),
         cookingRoutes = require('./routes/cookings'),
         indexRoutes   = require('./routes/index');
 
-
-
+let url = process.env.DATABASEURL || "mongodb://localhost:27017/Learn_And_Teach_Cooking";
+mongoose.connect(url);
 // mongoose.connect('mongodb://localhost:27017/Learn_And_Teach_Cooking', {useNewUrlParser: true});
-mongoose.connect('mongodb://adewaleshittu:Shetima5717@ds251894.mlab.com:51894/cookandfries', {useNewUrlParser: true});
+// mongoose.connect('mongodb://adewaleshittu:Shetima5717@ds251894.mlab.com:51894/cookandfries', {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
